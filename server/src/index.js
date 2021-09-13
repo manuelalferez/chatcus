6c05180bcfae3cfa135264793a8dfe9ec91192ee
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-13 10:53:01 +0200

    Integrating router

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,13 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
 
 server.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}}`);
