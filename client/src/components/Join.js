d11947eedfa63e7e3312a1bc33f720e1ee923047
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-14 10:25:27 +0200

    Adding input and button logic

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-5xl mt-40 mb-5">Join</h1>
       <div className="flex flex-col text-lg">
-        <input placeholder="Name" className="w-70 mb-2 p-2 bg-red-100 outline-none" type="text"></input>
-        <input placeholder="Room" className="w-70 p-2 bg-red-100 outline-none" type="text"></input>
+        <input
+          placeholder="Name"
+          className="w-70 mb-2 p-2 bg-red-100 outline-none"
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <input
+          placeholder="Room"
+          className="w-70 p-2 bg-red-100 outline-none"
+          type="text"
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        ></input>
       </div>
-      <Link>
-        <button className="bg-green-700 text-white mt-4 p-4 rounded-md hover:bg-green-900" type="submit">
+      <Link
+        onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+        to={`/chat?${name}&${room}`}
+      >
+        <button
+          className="bg-green-700 text-white mt-4 p-4 rounded-md hover:bg-green-900"
+          type="submit"
+        >
           Sign in
         </button>
       </Link>
