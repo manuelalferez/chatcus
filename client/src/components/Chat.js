9b5f59a60ed130a76ec1a61b6476497f84584287
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-15 15:53:42 +0200

    Adding users. Sending welcome message to client. Sending input to server

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -18,7 +20,11 @@ const Chat = ({ location }) => {
       transports: ["websocket"],
     });
 
-    socket.emit("join", { name, room });
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
 
     return () => {
       socket.emit("disconnect");
@@ -26,7 +32,35 @@ const Chat = ({ location }) => {
     };
   }, [ENDPOINT, location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+  }, []);
+
+  const sendMesssage = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input
+        value={message}
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+        onKeyPress={(event) =>
+          event.key === "Enter" ? sendMesssage(event) : null
+        }
+      />
+    </div>
+  );
 };
 
 export default Chat;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,12 +12,29 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-  socket.on("join", ({ name, room }) => {
-    console.log(name, room);
+    if (error) {
+      return callback(error);
+    }
+    socket.join(user.room);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name} welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    callback();
   });
-
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
