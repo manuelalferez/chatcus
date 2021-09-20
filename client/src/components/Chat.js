9f709342236ef33d3ffc635a4de4e3db44919768
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-09-20 16:54:39 +0200

    Adding endpoint to deploy

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -12,7 +12,7 @@ const Chat = ({ location }) => {
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://chatcus.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
