ef7add1a0481466d54fc3f84e5a77c7882c23991
ayeolakenny <ayeolakenny@gmail.com>
2021-10-07 11:19:19 +0100

    added chat emoji feature

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import sendIcon from "../../icons/sendIcon.png";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 const Input = ({ message, setMessage, sendMessage }) => {
+  const [showEmoji, setShowEmoji] = useState(false);
   return (
     <form className="flex justify-between items-center bg-white rounded-b-xl shadow-xl w-5/6 md:w-4/6 lg:w-3/6 lg:w-3/6 border-t-4 border-blue-50">
       <input
@@ -14,6 +17,17 @@ const Input = ({ message, setMessage, sendMessage }) => {
         }
         className="p-4 w-full outline-none rounded-bl-xl"
       />
+      {showEmoji && (
+        <span className="absolute bottom-28 right-36">
+          <Picker
+            onSelect={(e) => setMessage(message + e.native)}
+            emojiTooltip={true}
+          />
+        </span>
+      )}
+      <p onClick={() => setShowEmoji(!showEmoji)} className="cursor-pointer">
+        {String.fromCodePoint(0x1f60a)}
+      </p>
       <button
         onClick={(event) => sendMessage(event)}
         className="p-4 rounded-br-xl"
