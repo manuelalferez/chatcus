36429148f950a2220116ea1285f4c7696c4149fc
Vishal Sharma <vishalagnihotri345@gmail.com>
2021-10-07 10:20:44 +0530

    Modify Join component to parse room from query string

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
 import { Link } from "react-router-dom";
 
-const Join = () => {
+const Join = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-
+  useEffect(() => {
+    const { room } = queryString.parse(location.search);
+    console.log(queryString.parse(location.search));
+    if (room) {
+      setRoom(room);
+    }
+  }, []);
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="sm:text-3xl md:text-5xl mt-40 mb-10 ">
@@ -23,6 +30,7 @@ const Join = () => {
           placeholder="Room"
           className="w-70 p-2 bg-green-100 outline-none"
           type="text"
+          value={room}
           onChange={(event) => {
             setRoom(event.target.value);
           }}
