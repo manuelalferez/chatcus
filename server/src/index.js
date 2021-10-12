415b7d969311ae59affa243a285db7ac2cff7918
Sattwik <sahusattwik@gmail.com>
2021-10-12 22:23:25 +0530

    Added dynamic server URL

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const router = require("./router");
 
